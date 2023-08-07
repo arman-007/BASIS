1e1e2d01735021166ea4ea91f736596d26462e49
arman-007 <armanhosen9@gmail.com>
2023-08-07 13:03:17 +0600

    14 dB created and product add page initialized

---
+++ PHP/CRUD/admin/brands/create.php
@@ -0,0 +1,35 @@
+<?php
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  </head>
+  <body>
+    <section>
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-sm-6">
+                    <h1 class="text-center fs-3 fw-bolder mt-3 mb-4">Add New Brand</h1>
+                    <form action="store.php" method="post" enctype="multipart/form-data">
+                        <div class="mb-3 row">
+                            <label for="inputTitle" class="col-sm-3 col-form-label">Title:</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="inputTitle" name="title" value="" required>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-secondary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ PHP/CRUD/admin/brands/store.php
@@ -0,0 +1,28 @@
+<pre>
+    <?php
+    // print_r($_POST);
+
+    // echo $_POST['title'];
+    $_title = $_POST['title'];
+
+    // connection to DB
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    $conn = new PDO("mysql:host=$servername;dbname=ARMAN", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $query = "INSERT INTO `products` (`title`) VALUES (:title)";
+
+    $stmt = $conn->prepare($query);
+
+    $stmt -> bindParam(':title', $_title);
+
+    $result = $stmt -> execute();
+
+    var_dump($result);
+
+?>
+</pre>
